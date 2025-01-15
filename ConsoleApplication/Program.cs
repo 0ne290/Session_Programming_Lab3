b44385b14f0c51c22a9c2f799a8f55d609ebe053
One290 <lenya.dorovskoy@mail.ru>
2025-01-15 13:34:35 +0800

    init

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,32 @@
+﻿using Core;
+
+namespace ConsoleApplication;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        Console.Write("Enter the numbers: ");
+        SinglyLinkedList numbers;
+        try
+        { 
+            numbers = new SinglyLinkedList(Console.ReadLine()!);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            
+            Console.Write("To terminate the program, press any key...");
+            Console.ReadKey();
+            
+            return;
+        }
+        
+        Console.WriteLine($"Numbers: {numbers}.");
+        
+        Console.WriteLine($"Numbers without primes: {numbers.RemovePrimeNumbers()}.");
+        
+        Console.Write("To terminate the program, press any key...");
+        Console.ReadKey();
+    }
+}
---
+++ Core/SinglyLinkedList.cs
@@ -0,0 +1,81 @@
+﻿namespace Core;
+
+public class SinglyLinkedList
+{
+    private class Node
+    {
+        public required int Value { get; init; }
+
+        public Node? Next { get; set; }
+    }
+    
+    // Можно было бы построить список за один проход, начав с хвоста, а не с головы.
+    public SinglyLinkedList(string text)
+    {
+        var nodes = text.Split(",").Select(number => new Node { Value = Math.Abs(int.Parse(number.Trim())) }).ToList();
+
+        for (var i = 0; i < nodes.Count - 1; i++)
+            nodes[i].Next = nodes[i + 1];
+
+        _head = nodes[0];
+    }
+
+    public SinglyLinkedList RemovePrimeNumbers()
+    {
+        var current = _head;
+        Node prev = null!;
+        
+        while (current != null)
+        {
+            if (IsPrime(current.Value))
+            {
+                if (current == _head)
+                {
+                    _head = current.Next;
+                    current = _head;
+                    
+                    continue;
+                }
+                
+                prev.Next = current.Next;
+                current = current.Next;
+                
+                continue;
+            }
+
+            prev = current;
+            current = current.Next;
+        }
+
+        return this;
+    }
+
+    public static bool IsPrime(int number)
+    {
+        if (number is 0 or 1)
+            return false;
+        
+        for (var i = 2; i <= number / 2; i++)
+            if (number % i == 0)
+                return false;
+
+        return true;
+    }
+
+    public override string ToString()
+    {
+        var ret = string.Empty;
+        var current = _head;
+
+        while (current != null)
+        {
+            ret += $"{current.Value}, ";
+
+            current = current.Next;
+        }
+
+        return ret == string.Empty ? ret : ret[..^2];
+    }
+
+    private Node? _head;
+}
